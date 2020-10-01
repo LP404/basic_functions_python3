5622941cd499eec3d2cc53312ca5a3182124dccf
LP404 <71828222+LP404@users.noreply.github.com>
2020-10-01 14:50:16 +0100

    Added commentary for def gauss()

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,12 @@
 import numpy as np
 
+#Function takes in an array of data and outputs a sorted verson of the array
+#It also runs the array through a gaussian function that produces data for a yaxis
+#When the outputs are plotted it will produce a gaussian distribution
+
 def gauss(xarray):
     sorted_xarray = np.sort(xarray)
-    
+
     mean = np.mean(sorted_xarray)
     std = np.std(sorted_xarray)
     yarray = np.ones_like(sorted_xarray)
